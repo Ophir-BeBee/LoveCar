87ba51378780d8d72fb1ae4a3a7b8ab0e0a4c0e2
BeBee <originalbebee@gmail.com>
2024-03-02 10:51:34 +0630

    Merge branch 'shop-' of https://github.com/Ophir-BeBee/LoveCar into shop-

--- app/Http/Controllers/SaveController.php
+++ app/Http/Controllers/SaveController.php
@@ -7,8 +7,6 @@ use App\Models\Save;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
-use App\Http\Requests\SaveCreateRequest;
-use App\Http\Requests\SaveDeleteRequest;
 
 class SaveController extends Controller
 {
